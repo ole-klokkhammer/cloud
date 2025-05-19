571a444e3a13058418e40faf7c692d63f90d95de
Ole Klokkhammer <ole.klokkhammer@outlook.com>
2025-05-19 10:24:05 +0200

    sign in sign out

--- apps/neo/neo-web/src/auth/auth.config.ts
+++ apps/neo/neo-web/src/auth/auth.config.ts
@@ -6,16 +6,14 @@ export const authConfig = {
     },
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
-            console.log('authorized:', auth);
             const isLoggedIn = !!auth?.user;
-            const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-            if (isOnDashboard) {
-                if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
-            } else if (isLoggedIn) {
-                return Response.redirect(new URL('/dashboard', nextUrl));
+            const isLoginPage = nextUrl.pathname.startsWith('/login');
+
+            if (!isLoggedIn && !isLoginPage) {
+                return Response.redirect(new URL('/login', nextUrl));
+            } else {
+                return true;
             }
-            return true;
         },
     },
     providers: [], // Add providers with an empty array for now

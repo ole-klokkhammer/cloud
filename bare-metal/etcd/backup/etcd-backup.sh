0c8c289dc4a5635e7fc616b8fa9f776e19f395bf
ole.klokkhammer <ole.klokkhammer@outlook.com>
2025-06-22 22:24:12 +0200

    improves upon backup by removing old

--- bare-metal/etcd/backup/etcd-backup.sh
+++ bare-metal/etcd/backup/etcd-backup.sh
@@ -5,12 +5,17 @@
 set -x
 
 BUCKET_NAME="etcd"
+AWS_PROFILE="etcd-backup"
 TIMESTAMP=$(date +%s)
-SNAPSHOT="/tmp/etcd-snapshot-$TIMESTAMP"
+SNAPSHOT_PREFIX="etcd-snapshot"
+SNAPSHOT_NAME="$SNAPSHOT_PREFIX-$TIMESTAMP"
+SNAPSHOT_PATH="/tmp/$SNAPSHOT_NAME"
+SNAPSHOT_KEEP=7
 ENDPOINT_URL="https://j8t7.ldn203.idrivee2-94.com"
+HEALTHCHECKS_URL="http://192.168.10.204:8000/ping/55fe5c54-ca9c-431d-84cc-824376dbe8a4"
 
 export ETCDCTL_API=3 
-etcdctl_output=$(etcdctl --endpoints=http://127.0.0.1:2379 snapshot save "$SNAPSHOT" 2>&1)
+etcdctl_output=$(etcdctl --endpoints=http://127.0.0.1:2379 snapshot save "$SNAPSHOT_PATH" 2>&1)
 ETCD_STATUS=$?
 if [ $ETCD_STATUS -ne 0 ]; then
   echo "etcdctl snapshot failed"
@@ -20,7 +25,7 @@ fi
  
 # aws_output=$(aws s3 cp "$SNAPSHOT" s3://$BUCKET_NAME --profile etcd-backup --endpoint-url https://j8t7.ldn203.idrivee2-94.com 2>&1)
 # force single part upload to avoid multipart issues
-aws_output=$(aws s3api put-object --bucket $BUCKET_NAME --key $(basename "$SNAPSHOT") --body "$SNAPSHOT" --profile etcd-backup --endpoint-url "$ENDPOINT_URL" 2>&1)
+aws_output=$(aws s3api put-object --bucket $BUCKET_NAME --key "$SNAPSHOT_NAME" --body "$SNAPSHOT_PATH" --profile "$AWS_PROFILE" --endpoint-url "$ENDPOINT_URL" 2>&1)
 AWS_STATUS=$?
 if [ $AWS_STATUS -ne 0 ]; then
   echo "aws s3 upload failed"
@@ -28,11 +33,33 @@ if [ $AWS_STATUS -ne 0 ]; then
   exit $AWS_STATUS
 fi
 
-rm "$SNAPSHOT"
+echo "Cleanup: keep only the latest $SNAPSHOT_KEEP snapshots in the bucket"
+SNAPSHOT_LIST=$(aws s3api list-objects-v2 --bucket $BUCKET_NAME --profile "$AWS_PROFILE" --endpoint-url "$ENDPOINT_URL" --query 'Contents[].Key' --output json | jq -r ".[] | select(startswith(\"$SNAPSHOT_PREFIX\"))" | sort -n)
+SNAPSHOT_COUNT=$(echo "$SNAPSHOT_LIST" | wc -l)
 
-echo "Notify healthchecks on success"
-curl -fsS http://192.168.10.204:8000/ping/55fe5c54-ca9c-431d-84cc-824376dbe8a4
+if [ "$SNAPSHOT_COUNT" -gt "$SNAPSHOT_KEEP" ]; then
+  SNAPSHOTS_TO_DELETE=$(echo "$SNAPSHOT_LIST" | head -n -"$SNAPSHOT_KEEP")
+  for OLD_SNAPSHOT in $SNAPSHOTS_TO_DELETE; do
+    echo "Deleting old snapshot: $OLD_SNAPSHOT"
+    aws s3api delete-object --bucket $BUCKET_NAME --key "$OLD_SNAPSHOT" --profile "$AWS_PROFILE" --endpoint-url "$ENDPOINT_URL"
+    echo "Deleted old snapshot: $OLD_SNAPSHOT"
+  done
+fi
+rm "$SNAPSHOT_PATH"
 
 
-echo "backup completed successfully."
-exit 0
+echo "Check if the snapshot still exists in the bucket"
+aws s3api head-object --bucket "$BUCKET_NAME" --key "$SNAPSHOT_NAME" --profile "$AWS_PROFILE" --endpoint-url "$ENDPOINT_URL" > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  echo "Snapshot $SNAPSHOT_NAME has been successfully stored in the bucket."
+  curl -fsS "$HEALTHCHECKS_URL"
+  echo "backup completed successfully."
+  exit 0
+else
+  echo "Snapshot $SNAPSHOT_NAME does not exist in the bucket."
+  curl -fsS "$HEALTHCHECKS_URL/fail"
+  exit 1
+fi
+
+
+

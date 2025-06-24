e7d727fd2a47fbd9aeb60cb49397670ddbae7a14
ole.klokkhammer <ole.klokkhammer@outlook.com>
2025-06-24 12:40:23 +0200

    fs mount check

--- apps/hivemq/extensions/hivemq-allow-all-extension/build_and_push_to_host.sh
+++ apps/hivemq/extensions/hivemq-allow-all-extension/build_and_push_to_host.sh
@@ -3,4 +3,4 @@
 echo "Building and pushing allow-all extension"
 ./gradlew clean hivemqExtensionZip
 scp -r ./build/hivemq-extension/hivemq-allow-all-extension-*.zip ubuntu@192.168.10.2:/var/lib/hivemq/hivemq-allow-all-extension.zip
-ssh ubuntu@192.168.10.2 "cd /var/lib/hivemq && unzip hivemq-allow-all-extension.zip"
+ssh ubuntu@192.168.10.2 "cd /var/lib/hivemq/extensions && unzip hivemq-allow-all-extension.zip"
